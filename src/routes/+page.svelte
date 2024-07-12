090d42b5a26d18b9897664ef91ad07d8c511498c
Asmaa <asmaa.bajned@gmail.com>
2024-07-12 05:16:14 +0300

    nested squares

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>
+        calculator
+    </title>
+</svelte:head>
+      
+    <div class=" flex justify-center items-center bg-white h-[25rem] w-[18rem] rounded-2xl">
+       <div class="  bg-slate-700 h-[5rem] w-[5rem] rounded-3xl">
+
+       </div>
+    </div>
+  
