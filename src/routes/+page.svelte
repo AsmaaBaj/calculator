4feb0159f29126a04c656ea07587ffa5ec9fccd9
Asmaa <asmaa.bajned@gmail.com>
2024-07-17 18:06:38 +0300

    first draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,31 @@
     </title>
 </svelte:head>
       
-    <div class=" flex justify-center items-center bg-white h-[25rem] w-[18rem] rounded-2xl">
-       <div class="  bg-slate-700 h-[5rem] w-[5rem] rounded-3xl">
+    <div class="  bg-white rounded-2xl grid grid-cols-4 gap-1">
+        <button class="bg-slate-500">%</button>
+        <button class="bg-slate-500">√</button>
+        <button class="bg-slate-500">CE</button>
+        <button class="bg-red-700">C</button>
 
-       </div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button class="bg-[#7e7d39b0]">-</button>
+
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button class="bg-[#fffb1e]">÷</button>
+
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button class="bg-[#7e7d39b0]">×</button>
+
+        <button>.</button>
+        <button>0</button>
+        <button>=</button>
+        <button class="bg-[#fffb1e]">+</button>
+
     </div>
   
