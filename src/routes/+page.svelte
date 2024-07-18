c8d7ca8a5b0fef001863ff3d0dab4c244088f4c6
Asmaa <asmaa.bajned@gmail.com>
2024-07-19 01:55:05 +0300

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,33 +4,34 @@
     </title>
 </svelte:head>
       
-    <div class="  bg-white rounded-3xl grid grid-cols-4 gap-1 p-3 border-4 border-[#7e7d39b0] font-semibold">
+    <div class="  bg-white rounded-3xl grid grid-cols-4 gap-1 p-3 border-4 border-[#7e7d39b0] 
+    font-semibold shadow-2xl sh">
         <div class="bg-[#fffb1ebb] rounded-full col-span-4 h-12 flex items-center px-3 
         mb-2 border-[3px] border-[#7e7d3974]">812345</div>
         <button class="bg-slate-500">%</button>
         <button class="bg-slate-500">√</button>
         <button class="bg-slate-500">CE</button>
-        <button class="bg-red-700 text-lg">C</button>
+        <button class="bg-red-700 text-white">C</button>
 
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button class="bg-[#7e7d39b0]">-</button>
+        <button class="bg-[#7e7d39b0] ">-</button>
 
         <button>4</button>
         <button>5</button>
         <button>6</button>
-        <button class="bg-[#fffb1e]">÷</button>
+        <button class="bg-[#fffb1e] ">÷</button>
 
         <button>1</button>
         <button>2</button>
         <button>3</button>
-        <button class="bg-[#7e7d39b0]">×</button>
+        <button class="bg-[#7e7d39b0] ">×</button>
 
         <button>.</button>
         <button>0</button>
         <button class="bg-slate-500">=</button>
-        <button class="bg-[#fffb1e]">+</button>
+        <button class="bg-[#fffb1e] ">+</button>
 
     </div>
   
