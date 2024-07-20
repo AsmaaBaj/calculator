8b6926f6f13bf6d1f8f097873548258bef9fe129
Asmaa <asmaa.bajned@gmail.com>
2024-07-21 01:43:17 +0300

    size and color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </svelte:head>
       
     <div class="  bg-white rounded-3xl grid grid-cols-4 gap-1 p-3 border-4 border-[#7e7d39b0] 
-    font-semibold shadow-2xl sh">
+    font-semibold shadow-2xl ">
         <div class="bg-[#fffb1ebb] rounded-full col-span-4 h-12 flex items-center px-3 
         mb-2 border-[3px] border-[#7e7d3974]">812345</div>
         <button class="bg-slate-500">%</button>
