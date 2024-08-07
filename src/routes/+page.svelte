85ed8e9de8241d319ff957f7532ebbba1dabc11a
Asmaa <asmaa.bajned@gmail.com>
2024-08-07 22:38:18 +0300

    the program

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,17 @@
     }
     
     function BackSpace(){
-       equation=equation.substring(0, equation.length-1);
+        switch(equation.substring(equation.length-3, equation.length)){
+            case " + ":
+            case " - ":
+            case " / ":
+            case " * ":
+            equation = equation.substring(0, equation.length-3)
+              break;
+        default:   
+            equation = equation=equation.substring(0, equation.length-1);
+        }
+       
     }
 
     function clear(){
@@ -34,9 +44,12 @@
          
 
     <div class="  bg-white rounded-3xl grid grid-cols-4 gap-1 p-3 border-4 border-[#7e7d39b0] 
-    font-semibold shadow-2xl ">
+    font-semibold shadow-2xl max-w-[14rem]" > 
+
         <div class="bg-[#fffb1ebb] rounded-full col-span-4 h-12 flex items-center px-3 
-        mb-2 border-[3px] border-[#7e7d3974]"> {equation} </div>
+        mb-2 border-[3px] border-[#7e7d3974] overflow-auto"> 
+        {equation}
+     </div>
         <button class="bg-slate-500 text-xl" on:click={()=>addToEquatoin(" / 100")}>
             <Percent/>
          </button>
